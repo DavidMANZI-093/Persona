265a9b0b28e36ef99e40751b36cb254e338608c6
David MANZI <manzidavid093@gmail.com>
2024-09-08 19:10:39 +0200

    ...

--- templates/loading/loading.js
+++ templates/loading/loading.js
@@ -1,3 +1,18 @@
+// --- Loading Script --- //
+const logo = document.querySelector('.logo');
 const placeHolder = document.querySelector('.placeholder');
+const image_url = window.getComputedStyle(logo).backgroundImage.slice(5, -2); // Trying to extract the logo's url
 const ghost = document.querySelector('.ghost');
-placeHolder.addEventListener('')
+const p_logo = new Image();
+
+p_logo.src = image_url;
+
+p_logo.addEventListener('load', () => {
+    placeHolder.remove();
+});
+
+// --- EOS --- //
+
+const progress = document.querySelector('.progress');
+
+console.log(progress);
